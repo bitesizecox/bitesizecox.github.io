45953afc81e10a333204dd9902d276ad3bce3a5a
Meaghan Cox <mkcox@umich.edu>
2018-04-11 19:19:20 -0400

    trying to host some original code

---
+++ Portfolio/js/script.js
@@ -0,0 +1,42 @@
+// function() {
+		// var selectedClass = "";
+// 		"portfolio-btn".click(function(){ 
+// 			selectedClass = $(this).attr("data-rel"); 
+//      	".portfolio-content".fadeTo(100, 0.1);
+// 		".portfolio div".not("."+selectedClass).fadeOut().removeClass('work');
+//     	setTimeout(function() {
+//      		"."+selectedClass.fadeIn().addClass('work');
+//      		".portfolio-content".fadeTo(300, 1);
+//      	}, 300); 
+		
+// 	});
+
+
+// $(function() {
+// 		var selectedClass = "";
+// 		$(".fil-cat").click(function(){ 
+// 		selectedClass = $(this).attr("data-rel"); 
+//      $("#portfolio").fadeTo(100, 0.1);
+// 		$("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+//     setTimeout(function() {
+//       $("."+selectedClass).fadeIn().addClass('scale-anm');
+//       $("#portfolio").fadeTo(300, 1);
+//     }, 300); 
+		
+// 	});
+// });
+
+
+//
+// Description: Randomly change background color every 5 seconds
+//
+// NewcWare 1997
+// Author: Scott Newcomer 3/1997
+// Email: nuke@bright.net
+// //
+function showImage(image){
+		document.getElementById('p-image').style.visibility="visible";
+	}
+	function hideImage(image){
+		document.getElementById('p-image').style.visibility="hidden";
+	}
